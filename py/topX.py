9a15db7a3c46daa09ad7cc695db54e35c667efd7
LiDong <jellylidong2nd@gmail.com>
2015-11-15 18:16:41 -0500

    Optimized XPerSec function, add protocolPerSec

--- py/topX.py
+++ py/topX.py
@@ -72,6 +72,114 @@ import datetime
 def unix2readable(t): # t is string
 	return datetime.datetime.fromtimestamp(int(float(t))).strftime('%m-%d-%Y %H:%M:%S')
 
+#time realated: pkts/sec, bytes/sec
+# def rateFunc(rawRdd):
+# 	pktsRate = []
+# 	dataRate = []
+# 	timeline = []
+# 	sec = 0
+# 	lo = float(rawRdd.take(1)[0][5])
+# 	hi = lo + 1.0
+# 	end = float(rawRdd.collect()[-1][5])
+# 	flag = True
+# 	while(flag):
+# 		tmpRdd = rawRdd.filter(lambda x: float(x[5]) >= lo and float(x[5]) < hi)
+# 		pr = tmpRdd.count()
+# 		dr = tmpRdd.map(lambda x: float(x[2])).sum()
+# 		timeline.append(sec)
+# 		pktsRate.append(pr)
+# 		dataRate.append(dr)
+# 		if hi >= end:
+# 			flag = False
+# 		lo = hi
+# 		hi += 1.0
+# 		sec += 1.0
+#
+# 		print '********************************************************'
+# 		print '********************************************************'
+# 		print '************************' + str(lo) + '******************************'
+# 		print '************************' + str(sec) + '******************************'
+# 		print '************************' + str(hi) + '******************************'
+# 		print '********************************************************'
+# 		print '********************************************************'
+# 	return (pktsRate, dataRate, timeline)
+
+# src, dst, data_length, protocol_name, protocol_number, arrival_time (len = 6)
+def pktPerSec(rawRdd):
+	tmpRdd = rawRdd.map(lambda x: (int(float(x[5])), 1)).groupByKey().map(lambda (k,v): (k,sum(v)))
+	rate = tmpRdd.map(lambda (k,v): v).collect()
+	time = tmpRdd.map(lambda (k,v): k).collect()
+	return (rate, time)
+
+def dataPerSec(rawRdd):
+	tmpRdd = rawRdd.map(lambda x: (int(float(x[5])), float(x[2]))).groupByKey().map(lambda (k,v): (k,sum(v)))
+	rate = tmpRdd.map(lambda (k,v): v).collect()
+	time = tmpRdd.map(lambda (k,v): k).collect()
+	return (rate, time)
+
+def protocolPerSec(rawRdd):
+	res = []
+	protocolList = rawRdd.map(lambda x: x[3]).distinct().collect()
+	protocolRdd = rawRdd.map(lambda x: (int(float(x[5])), x[3])) #(time, protocol)
+	time = protocolRdd.map(lambda (t, p): t).collect()
+	def mapProtocol(p, target):
+		if p == target:
+			return 1
+		return 0
+	for protocol in  protocolList:
+		tmpRdd = protocolRdd.map(lambda (t,p): (t, mapProtocol(p, protocol))).groupByKey().map(lambda (k,v): (k, sum(v)))
+		rate = tmpRdd.map(lambda (k,v): v).collect()
+		#time = tmpRdd.map(lambda (k,v): k).collect()
+		res.append((protocol, rate))
+	return (res, time)
+
+# normalPR, normalDR, normalX = rateFunc(normalRaw)
+# attackPR, attackDR, attackX = rateFunc(attackRaw)
+normalPR, normalX = pktPerSec(normalRaw)
+normalDR, normalX = dataPerSec(normalRaw)
+attackPR, attackX = pktPerSec(attackRaw)
+attackPR, attackX = dataPerSec(attackRaw)
+
+normalProR, normalX = protocolPerSec(normalRaw) #([(pro, rate)], time)
+attackProR, attackX = protocolPerSec(attackRaw)
+
+
+# import matplotlib.pyplot as plt
+# plt.plot(normalProR[2][1])
+# plt.plot()
+# plt.show()
+
+AveragePacketRateSchema = {
+	'normal_X': normalX,
+	'attack_X': attackX,
+	'normal_Average': float(sum(normalPR))/len(normalPR),
+	'attack_Average': float(sum(attackPR))/len(attackPR),
+	'normal_min': min(normalPR),
+	'attack_min': min(attackPR),
+	'normal_max': max(normalPR),
+	'attack_max': max(attackPR),
+	'normal_Y': normalPR,
+	'attack_Y': attackPR
+}
+
+AverageDataRateSchema = {
+	'normal_X': normalX,
+	'attack_X': attackX,
+	'normal_Average': float(sum(normalDR))/len(normalDR),
+	'attack_Average': float(sum(attackDR))/len(attackDR),
+	'normal_min': min(normalDR),
+	'attack_min': min(attackDR),
+	'normal_max': max(normalDR),
+	'attack_max': max(attackDR),
+	'normal_Y': normalDR,
+	'attack_Y': attackDR
+}
+
+ProtocolDistributionSchema = {
+
+}
+
+
 
 
 normalTotalData = normalRaw.map(lambda x:float(x[2])).sum()
